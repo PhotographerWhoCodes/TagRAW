5b8000a348d8c6746c9a388a3b37b6b48d11aa48
PhotographerWhoCodes <130889072+PhotographerWhoCodes@users.noreply.github.com>
2023-04-15 23:08:17 -0700

    Works for local files on OS X but doesn't work for SD card formatted exFAT.

---
+++ tag.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Check if two arguments were provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 [filepath A] [filepath B]"
+    exit 1
+fi
+
+# Extract the directory paths and check if they exist
+dirA=$(dirname "$1")
+dirB=$(dirname "$2")
+if [ ! -d "$dirA" ]; then
+    echo "Error: $dirA is not a valid directory"
+    exit 1
+fi
+if [ ! -d "$dirB" ]; then
+    echo "Error: $dirB is not a valid directory"
+    exit 1
+fi
+
+# Find all yellow files in filepath A
+# yellow_files=$(mdfind "kMDItemUserTags == 'Yellow'" -onlyin "$dirA" | xargs -I{} basename {} | sed 's/\.[^.]*$//')
+yellow_files=$(mdfind "kMDItemUserTags == 'Yellow'" -onlyin "$dirA" | xargs -I{} basename {} | sed 's/\.[^.]*$//')
+echo "Yellow files in directory A:"
+echo "$yellow_files"
+
+# Check if there are any yellow files
+if [ -z "$yellow_files" ]; then
+    echo "No yellow files found in $dirA"
+    exit 0
+fi
+
+# Tag orange files in filepath B that have the same name (without extension) as any yellow file
+find "$dirB" -type f -print0 | while read -d $'\0' file; do
+    filename=$(basename "$file")
+    name=$(echo "$filename" | cut -f 1 -d '.')
+    if echo "$yellow_files" | grep -q "$name"; then
+        osascript -e "tell application \"Finder\" to set label index of (POSIX file \"$file\" as alias) to 2"
+        echo "Tagged $filename in $dirB"
+    fi
+done
